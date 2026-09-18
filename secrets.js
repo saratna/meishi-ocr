421a4379d8c354f5fd4a85a7e5adb42bd32ddd52
saratna <ryuji@saratna.com>
2026-09-18 20:24:48 +0900

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -6,6 +6,6 @@
 //   誤り例:   https://script.google.com/macros/library/d/...  ← これはライブラリ用
 
 window.MEISHI_OCR = {
-  GAS_URL: 'https://script.google.com/macros/library/d/1nfilxF4dTWCQcWLQPqo29p9HQr5zz0esT7scenA4pJbg_FPzfP0ktCVC/8',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzGuDkmbTBIMD_ntmOZpD97RxXRsS-ehVI1iFnU8L6Ja-83LVr3Jpek3ih_9WRmQiw/exec',
   API_TOKEN: 'LgMwZY60hTyyQQd3zEQH0f72n7vHIHpaUxDdZhFN'
 };
