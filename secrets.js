09364034fcc509cf986f1cb4dfbec60293eac7cb
saratna <ryuji@saratna.com>
2026-09-18 20:19:22 +0900

    Update secrets.js

--- secrets.js
+++ secrets.js
@@ -2,6 +2,6 @@
 // GAS の CONFIG.API_TOKEN にも、下の API_TOKEN と「同じ文字」を入れてください
 
 window.MEISHI_OCR = {
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbzGuDkmbTBIMD_ntmOZpD97RxXRsS-ehVI1iFnU8L6Ja-83LVr3Jpek3ih_9WRmQiw/exec',
+  GAS_URL: 'https://script.google.com/macros/library/d/1nfilxF4dTWCQcWLQPqo29p9HQr5zz0esT7scenA4pJbg_FPzfP0ktCVC/7',
   API_TOKEN: 'LgMwZY60hTyyQQd3zEQH0f72n7vHIHpaUxDdZhFN'
 };
